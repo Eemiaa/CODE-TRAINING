687367e2dc76f6cc30d557bf5d7767889b9f8fa2
Aimeê Miranda <aimeemiranribe@gmail.com>
2022-06-09 12:00:49 -0300

    commit-01

---
+++ CODECAD/Passed_or_Failed.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+  float A, B, media;
+  cin >> A >> B;
+  media = (A+B)/2;
+  if(media >= 7){
+    cout << "Aprovado";
+  }
+  if(media < 7 and media >= 4){
+    cout << "Recuperacao";
+  }
+	if(media < 4){
+	  cout << "Reprovado";
+	}
+}
